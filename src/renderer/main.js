bea7745df719d370e52f408a055a2a30e8fa4880
Daniel Alejandro Chavira Cervantes <danielchavira1@gmail.com>
2018-06-21 23:03:39 -0500

    Removed css imports

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import ToggleButton from 'vue-js-toggle-button';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
 import App from './App';
 import router from './router';
 import store from './store';
