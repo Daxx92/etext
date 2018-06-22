4a8c3da0322d46dba34a12a68188624226c4cd38
Daniel Alejandro Chavira Cervantes <danielchavira1@gmail.com>
2018-06-21 23:05:13 -0500

    Added codemirror js files

---
+++ src/renderer/assets/js/currying.js
@@ -0,0 +1,10 @@
+﻿/* eslint-disable */
+export default function(CodeMirror){
+    var powerUp = (mode)=>{
+        mode(CodeMirror)
+        return function(){
+             if(arguments[0]) return powerUp(arguments[0]);
+         }
+      }
+      return powerUp;
+}
