e0e77e56b8ff0ba467826a2df68b1543977fcbfe
Daniel Alejandro Chavira Cervantes <danielchavira1@gmail.com>
2018-06-23 03:34:53 -0500

    Extract constants to a file

---
+++ src/utils/Constants.js
@@ -0,0 +1,8 @@
+export const FILE_EXTENSION = '.emd';
+
+export const SHOW_OPEN_DIALOG = 'SHOW_OPEN_DIALOG';
+export const SHOW_SAVE_DIALOG = 'SHOW_SAVE_DIALOG';
+export const FILE_READ = 'FILE_READ';
+export const FILE_WRITTEN = 'FILE_WRITTEN';
+export const FILE_READ_ERROR = 'FILE_READ_ERROR';
+export const FILE_WRITTEN_ERROR = 'FILE_WRITTEN_ERROR';
