02a3593bc84cfcda27bc17a27e84c117c23fb4e6
Daniel Alejandro Chavira Cervantes <danielchavira1@gmail.com>
2018-06-23 03:34:17 -0500

    Extract file operations to a class

---
+++ src/main/classes/FileManager.js
@@ -0,0 +1,23 @@
+import jetpack from 'fs-jetpack';
+
+export default class FileManager {
+  constructor() {
+    this.error = null;
+  }
+
+  static readFile(filePath) {
+    return jetpack.readAsync(filePath);
+  }
+
+  static writeFile(filePath, content) {
+    return jetpack.writeAsync(filePath, content, { atomic: true });
+  }
+
+  static appendExtensionToPath(filePath, extension) {
+    if (!filePath.endsWith(extension)) {
+      return `${filePath}${extension}`;
+    }
+
+    return filePath;
+  }
+}
