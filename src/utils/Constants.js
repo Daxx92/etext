7030c3f47efc667bec2ad7f0cd47373f0ae7b514
Daniel Alejandro Chavira Cervantes <danielchavira1@gmail.com>
2018-06-29 02:28:48 -0500

    Replaced read/write error constant with one

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -4,5 +4,4 @@ export const SHOW_OPEN_DIALOG = 'SHOW_OPEN_DIALOG';
 export const SHOW_SAVE_DIALOG = 'SHOW_SAVE_DIALOG';
 export const FILE_READ = 'FILE_READ';
 export const FILE_WRITTEN = 'FILE_WRITTEN';
-export const FILE_READ_ERROR = 'FILE_READ_ERROR';
-export const FILE_WRITTEN_ERROR = 'FILE_WRITTEN_ERROR';
+export const FILE_ERROR = 'FILE_ERROR';
