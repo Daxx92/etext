e699537305338b23686e46b3961ad8ddd8044814
Daniel Alejandro Chavira Cervantes <danielchavira1@gmail.com>
2018-06-29 02:30:21 -0500

    Added private & public key inputs

--- src/renderer/components/MarkdownEditor.vue
+++ src/renderer/components/MarkdownEditor.vue
@@ -24,7 +24,9 @@
                     <div class="col text-left">
                         <b-button class="m-0" size="sm" variant="success" @click="open"> Open File</b-button>
                         <b-button class="m-0" size="sm" variant="success" @click="saveFile"> Save File</b-button>
-                        <b-button class="m-0" size="sm" variant="success" @click="passphraseModal = !passphraseModal"> Set Passphrase </b-button>
+                        <b-button class="m-0" size="sm" variant="success" @click="passphraseModal = !passphraseModal">
+                            Set Passphrase
+                        </b-button>
                     </div>
                     <div class="col text-right">
                         <toggle-button class="m-0"
@@ -66,16 +68,13 @@
                 v-model="passphraseModal"
                 title="Set Passphares"
                 :ok-only="true"
-                @ok="validatePassphrase"
+                @ok="validateForm"
         >
-            <p>
-                Set the passphrase for this file. Make sure you remember it as it's necessary to decrypt it!
-            </p>
 
             <form @submit.stop.prevent="">
 
                 <b-form-group id="passphrase-input"
-                              label="Passphrase:"
+                              label="Set the passphrase for this file:"
                               label-for="passphrase"
                               description="At least 8 caracters.">
                     <b-form-input id="passphrase"
@@ -86,6 +85,31 @@
                                   placeholder="Enter Passhprase">
                     </b-form-input>
                 </b-form-group>
+
+                <b-form-group id="rsa-private-input"
+                              label="RSA Public key"
+                              label-for="rsa-private"
+                              description="Set your RSA public key.">
+                    <b-form-textarea id="rsa-private"
+                                     v-model.trim="publicRsaKey"
+                                     placeholder="RSA public key (.pub file)"
+                                     :rows="3"
+                                     :max-rows="6">
+                    </b-form-textarea>
+                </b-form-group>
+
+                <b-form-group id="textarea1-input"
+                              label="RSA Private key"
+                              label-for="textarea1"
+                              description="Set your RSA private key.">
+                    <b-form-textarea id="textarea1"
+                                     v-model.trim="privateRsaKey"
+                                     placeholder="RSA private file (.pem file)"
+                                     :rows="3"
+                                     :max-rows="6">
+                    </b-form-textarea>
+                </b-form-group>
+
             </form>
 
         </b-modal>
@@ -101,7 +125,7 @@
     import marked from 'marked';
     import highlight from 'highlight.js';
 
-    import { SHOW_OPEN_DIALOG, SHOW_SAVE_DIALOG, FILE_READ, FILE_WRITTEN } from '../../utils/Constants';
+    import { SHOW_OPEN_DIALOG, SHOW_SAVE_DIALOG, FILE_READ, FILE_WRITTEN, FILE_ERROR } from '../../utils/Constants';
 
     const ace = require('brace');
     require('brace/mode/markdown');
@@ -114,6 +138,10 @@
     });
     ipcRenderer.on(FILE_WRITTEN, (event, data) => {
       bus.$emit(FILE_WRITTEN, data);
+    });
+    
+    ipcRenderer.on(FILE_ERROR, (event, data) => {
+      bus.$emit(FILE_ERROR, data);
     });
 
     marked.setOptions({
@@ -141,39 +169,32 @@
           },
           passphraseModal: false,
           passphrase: '',
+          publicRsaKey: '',
+          privateRsaKey: '',
         };
-      },
-      computed: {
-        mode() {
-          return this.$store.state.mode;
-        },
       },
       methods: {
         open() {
-          if (!this.isPassphraseValid()) {
-            this.alert.visible = true;
-            this.alert.variant = 'danger';
-            this.alert.text = 'Passphrase has to be set to open the file!';
-            return;
-          }
+          if (this.isInputValid()) {
+            this.isLoading = true;
+            const payload = {
+              privateRsaKey: this.privateRsaKey,
+            };
 
-          this.isLoading = true;
-          const payload = this.passphrase;
-    
-          ipcRenderer.send(SHOW_OPEN_DIALOG, payload);
+            ipcRenderer.send(SHOW_OPEN_DIALOG, payload);
+          }
         },
         saveFile() {
-          if (!this.isPassphraseValid()) {
-            this.alert.visible = true;
-            this.alert.variant = 'danger';
-            this.alert.text = 'Passphrase has to be set to save the file!';
-            return;
+          if (this.isInputValid()) {
+            this.isLoading = true;
+            const payload = {
+              content: this.aceEditor.getValue(),
+              passphrase: this.passphrase,
+              publicRsaKey: this.publicRsaKey,
+            };
+
+            ipcRenderer.send(SHOW_SAVE_DIALOG, payload);
           }
-
-          this.isLoading = true;
-          const payload = { text: this.aceEditor.getValue(), passphrase: this.passphrase };
-
-          ipcRenderer.send(SHOW_SAVE_DIALOG, payload);
         },
         widthClass(visible) {
           return visible ? 'w-50' : 'w-100';
@@ -191,26 +212,52 @@
             this.editorVisible = true;
           }
         },
-        validatePassphrase(event) {
+        validateForm(event) {
           event.preventDefault();
-    
-          if (this.isPassphraseValid()) {
-            this.passphraseModal = false;
 
+          const valid = this.isInputValid();
+
+          if (valid) {
             this.alert.visible = true;
             this.alert.variant = 'success';
-            this.alert.text = 'Passphrase Saved';
-
-            return true;
+            this.alert.text = 'Data saved, you can now open & save files.';
+            this.passphraseModal = false;
           }
 
-          this.alert.visible = true;
-          this.alert.variant = 'danger';
-          this.alert.text = 'Passphrase has to be at least 8 characters long';
-          return false;
+          return valid;
+        },
+        isInputValid() {
+          if (!this.isPassphraseValid) {
+            this.alert.visible = true;
+            this.alert.variant = 'danger';
+            this.alert.text = 'Passphrase has to be set & at least 8 characters long';
+            return false;
+          } else if (!this.isRSAPrivateValid) {
+            this.alert.visible = true;
+            this.alert.variant = 'danger';
+            this.alert.text = 'Invalid Private RSA key';
+            return false;
+          } else if (!this.isRSAPublicValid) {
+            this.alert.visible = true;
+            this.alert.variant = 'danger';
+            this.alert.text = 'Invalid Public RSA key';
+            return false;
+          }
+          return true;
+        },
+      },
+      computed: {
+        mode() {
+          return this.$store.state.mode;
         },
         isPassphraseValid() {
           return this.passphrase.length >= 8;
+        },
+        isRSAPublicValid() {
+          return this.publicRsaKey.length >= 5; // Aprox lenght of a key?
+        },
+        isRSAPrivateValid() {
+          return this.privateRsaKey.length >= 5; // Aprox lenght of a key?
         },
       },
       mounted() {
@@ -243,18 +290,18 @@
             this.alert.text = 'File loaded!';
 
             this.isLoading = false;
-    
+
             /* ****************************************************
-            * @TODO: Large Files!
-            * Large files have a huge overhead, but somehow ~ 10mb is ok?
-            * Maybe some kind of separator or something that tells us where to split?
-            ***************************************************** */
+                    * @TODO: Large Files!
+                    * Large files have a huge overhead, but somehow ~ 10mb is ok?
+                    * Maybe some kind of separator or something that tells us where to split?
+                    ***************************************************** */
             /* console.log(fileContents.length);
-            const l = fileContents.length;
-            const ml = 10000000;
-            const t = l > ml ? fileContents.substring(0, ml) : fileContents;
-            this.aceEditor.setValue(t);
-            this.isLoading = false; */
+                    const l = fileContents.length;
+                    const ml = 10000000;
+                    const t = l > ml ? fileContents.substring(0, ml) : fileContents;
+                    this.aceEditor.setValue(t);
+                    this.isLoading = false; */
           } else {
             this.alert.variant = 'warning';
             this.alert.text = 'Select a file to open it in the editor.';
@@ -273,6 +320,13 @@
             this.alert.text = 'Select a file to save editor contents.';
             this.alert.visible = true;
           }
+          this.isLoading = false;
+        });
+
+        bus.$on(FILE_ERROR, (error) => {
+          this.alert.variant = 'danger';
+          this.alert.text = error;
+          this.alert.visible = true;
           this.isLoading = false;
         });
       },
