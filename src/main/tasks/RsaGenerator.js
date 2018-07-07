69d1a600b2ea0e6aee98165ff42b918fbd6235f0
Daniel Alejandro Chavira Cervantes <danielchavira1@gmail.com>
2018-07-07 04:12:00 -0500

    Removed unnecessary values

--- src/main/tasks/RsaGenerator.js
+++ src/main/tasks/RsaGenerator.js
@@ -1,13 +1,7 @@
 import BackgroundTask from './BackgroundTask';
 
 const HTML_FILE = 'rsa-generator.html';
-const OPTIONS = {
-  height: 600,
-  width: 400,
-  defaultEncoding: 'utf-8',
-  nodeIntegration: false,
-  show: true,
-};
+const OPTIONS = {}; // Default values are Ok
 const DOM_READY_EVENT_NAME = 'rsa.ready';
 
 export default new BackgroundTask(HTML_FILE, DOM_READY_EVENT_NAME, OPTIONS);
